b3defdcc7c2c3061d5dd8f70e53db4d33c1ac2be
Arish613 <gotechnicians.com@gmail.com>
2026-01-22 13:00:44 +0530

    improved performance by using external css and config in next

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    esmExternals: true, // Use ES modules for externals
+  },
   images: {
     remotePatterns: [
       {
